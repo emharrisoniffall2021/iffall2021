3d9a4b8ac12be41bb8e63e6400dd382ccd58cde2
emharrisoniffall2021 <89925892+emharrisoniffall2021@users.noreply.github.com>
2021-11-30 21:02:44 -0600

    Add files via upload

---
+++ microsite/fully-realized/scripts.js/scripts.js
@@ -0,0 +1,19 @@
+$(document) .ready(function() {
+
+                $(".hamburger").click(function(){
+                    $("#main-nav").toggleClass("open");
+                    console.log("Toggled the class!");
+                });
+
+
+});
+
+
+// function myFunction() {
+//     var x = document.getElementById("links");
+//     if (x.style.display === "block") {
+//       x.style.display = "none";
+//     } else {
+//       x.style.display = "block";
+//     }
+//   }
